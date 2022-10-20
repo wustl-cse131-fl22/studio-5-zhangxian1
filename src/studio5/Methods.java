b529ddce27f93ad22a920698bfcda0c688149272
olithesnailsmasher <olithesnailsmasher@localhost>
2022-10-20 12:46:11 -0500

    studio 5

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -16,7 +16,7 @@ public class Methods {
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		distance = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));		
 		return distance;
 	}
 
@@ -35,6 +35,7 @@ public class Methods {
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
 
+		//double 
 		
 
 		// Red ring with 1.0/2.0 the radius
